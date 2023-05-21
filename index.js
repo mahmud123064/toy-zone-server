06f57462a340b4d2655f2694f9d80e6f121255a0
Md. Mahmud Uz Zaman <zmahmud26@gmail.com>
2023-05-21 12:03:43 +0600

    get Data

--- index.js
+++ index.js
@@ -16,38 +16,43 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+
+        const toyCollection = client.db("toyTruck").collection("toys")
+
+        app.get("/alltoys", async (req, res) => {
+            const cursor = toyCollection.find();
+            const result = await cursor.toArray()
+            res.send(result);
+        })
 
 
 
-
-
-    
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req, res)=>{
-   res.send("Toy server is running");
+app.get('/', (req, res) => {
+    res.send("Toy server is running");
 })
 
-app.listen(port, (req, res)=> {
-console.log(`Toy server is running on port: ${port}`);
+app.listen(port, (req, res) => {
+    console.log(`Toy server is running on port: ${port}`);
 })
