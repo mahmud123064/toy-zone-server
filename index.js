9081f3812bc6197969ce25f0d713e6034e5859ee
Md. Mahmud Uz Zaman <zmahmud26@gmail.com>
2023-05-23 18:06:20 +0600

    done

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        client.connect();
+        // client.connect();
         // Send a ping to confirm a successful connection
 
         const toyCollection = client.db("toyTruck").collection("toys")
@@ -67,8 +67,6 @@ async function run() {
             const result = await toyCollection.deleteOne(query);
             res.send(result)
         })
-
-
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
