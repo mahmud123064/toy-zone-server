a16e38ba3303daba8ee7f3a9e462b365e10cb805
Md. Mahmud Uz Zaman <zmahmud26@gmail.com>
2023-05-23 13:31:47 +0600

    Deleted

--- index.js
+++ index.js
@@ -61,6 +61,13 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/alltoyss/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await toyCollection.deleteOne(query);
+            res.send(result)
+        })
+
 
 
         await client.db("admin").command({ ping: 1 });
